9b6bada87a2a03955117eb7213a33309eac81836
Deirdra Kiai <me@deirdrakiai.com>
2014-08-14 17:49:09 -0700

    Added a wee bit of content

--- assets/client.js
+++ assets/client.js
@@ -2,11 +2,11 @@
 // which you're running the server code, then have all the mobile devices connect to it.
 // Once you've done so, replace the IP address below with one self-assigned to your
 // computer. Everything should be nice and zippy, since it's all running locally.
-var socket = io.connect('http://169.254.189.246:8080');
+//var socket = io.connect('http://xxx.xxx.xxx.xxx:8080/');
 
 // Alternatively, you can use a tunnel to localhost. I use ngrok (ngrok.com).
 // You'll need all your devices to connect to reliable internet, though.
 //var socket = io.connect('http://performotron.ngrok.com');
 
 // If you just want to run stuff on one machine, just use localhost.
-//var socket = io.connect('http://localhost:8080');
+var socket = io.connect('http://localhost:8080');
