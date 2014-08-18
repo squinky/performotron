fad684daf84883e9a3db11c2478330fbcb58250c
Deirdra Kiai <me@deirdrakiai.com>
2014-08-18 13:10:02 -0700

    Music edits

--- assets/band.js
+++ assets/band.js
@@ -2,6 +2,6 @@ socket.on('updateMusic', function (data)
 {
 	var old = $("#band").html();
 	$("#band").html('');
-	if (data.line != old) flashScreen();
+	if (data.line && data.line != old) flashScreen();
 	$("#band").append(data.line);
 });
